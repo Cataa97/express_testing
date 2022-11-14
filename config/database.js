aafddd470f23ee4c239fbb4c1a48f384718974ab
Catalina <catalina.castillo19c@gmail.com>
2022-11-14 13:59:25 -0300

    nodemon and envs up!

--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
 import { Sequelize } from "sequelize";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const db = new Sequelize("chocolateria", "postgres", "12345",
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
-        define: {
-            timestamps: false
-        },
-    });
+const db = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    define: {
+        timestamps: false
+    },
+});
 
 
 
