f79b1818afe63ba33bdbd9a147da6f6a2889aedd
Catalina <catalina.castillo19c@gmail.com>
2022-11-24 14:07:17 -0300

    edit auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";  //generar token
 import User from "../models/user.js";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
     try {
