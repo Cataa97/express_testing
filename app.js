9d77f3eb4bcfeedf21826e3de51acaf8c97e64d1
Catalina <catalina.castillo19c@gmail.com>
2022-11-25 11:26:17 -0300

    Router

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import router from "./routes/routes.js";
 import db from "./config/database.js";
 import cors from "cors";
 import Router from "./routes/routes.js";
@@ -8,11 +7,11 @@ import helmet from "helmet";
 const app = express();
 app.use(express.json());
 
-// var corsOptions = {
-//   origin: 'http://example.com',
-// }
-app.use(helmet());
+console.log(process.env.ENV);
 
+app.use(express.json());
+
+app.use(helmet());
 
 app.use(cors());
 try {
@@ -22,9 +21,9 @@ try {
     console.error('Unable to connect to the database:');
 }
 
-app.use(router);
+app.use(Router);
 app.use(AuthRouter);
 
-app.listen(8080, () => console.log('Servidor corriendo en localhost 4000....'))
+app.listen(8080, () => console.log('Servidor corriendo....'))
 
 export default app;
