a247204f4737fdc0debe64cdb0e9f8c67af70d5f
Catalina <catalina.castillo19c@gmail.com>
2022-11-25 11:11:32 -0300

    test port

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ const config = {
     username: process.env.DB_TEST_USER || 'postgres',
     password: process.env.DB_TEST_PASSWORD || '12345',
     database: process.env.DB_TEST_DB_NAME || 'chocolateria',
-    host: process.env.DB_TEST_HOST || 'localhost',
+    host: process.env.DB_TEST_HOST || '127.0.0.1',
     port: process.env.DB_TEST_PORT,
     logging: false,
     dialect: 'postgres',
